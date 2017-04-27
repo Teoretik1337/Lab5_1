828b3f56764f0b80d7d3236d3852de81f8db1cf4
Teoretik1337 <goga_stalker@mail.ru>
2017-04-27 20:12:37 +0700

    Доработаны выводы

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -12,18 +12,17 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	list<long> myList; // объявляем пустой список
+	list<long> myList; // объявляем пустой список 1
 	srand(time(NULL));
 
 	for (int i = 0; i < 15; i++) 
 	{
 		myList.push_back(rand() % 20); // добавляем в список новые элементы
 	}
-
-	cout << "Список: ";
-	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));
+	cout << "List 1: ";
+	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));//вывод списка 1
 	cout << "\n";
-	//добавить слово ввод
+	
 	int val;
 	cout << "\nВведите номер удаляемого элемента: ";
 	cin >> val;
@@ -31,7 +30,7 @@ int main()
 	std::advance(range, val-1);
 	myList.erase(range);
 
-	cout << "\nСписок с удаленным элементом: ";
+	cout << "\nList 1 с удаленным элементом: ";
 	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));
 
 
@@ -43,19 +42,17 @@ int main()
 	{
 		myList2.push_back(rand() % 20); // добавляем в список 2 новые элементы
 	}
-	cout << "\nСписок 2: ";
+	cout << "\nList 2: ";
 	copy(myList2.begin(), myList2.end(), ostream_iterator<long>(cout, " "));
 	cout << "\n";
 
 	//////////////////////////////////////////////////////////////////////
-
 	int n;
-	cout << "\nВведите номер элемента с которого будем удалять: \n";
+	cout << "\nВведите номер элемента с которого будем удалять из List 1: \n";
 	cin >> n;
-	cout << "\nВведите количество \n";
+	cout << "\nВведите количество удаляемых элементов из List 1\n";
 	int count;
 	cin >> count;
-	
 	for (int i = 0; i < count; i++)
 	{
 		std::list<long>::iterator range = myList.begin();
@@ -63,18 +60,18 @@ int main()
 		myList.erase(range);
 	}
 
-	cout << "\nСписок с удаленными элементами: ";
+	cout << "\nList 1 с удаленными элементами: ";
 	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));
 	//////////////////////////////////////////////////////////////////////
 	
 	myList.sort();
 	myList2.sort();
 	myList.merge(myList2);
-	cout << "\nОбъединили списки в list 1: ";
+	cout << "\nОбъединили List 2 в list 1: ";
 	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));
 	cout << "\nЧто осталось в  list 2: ";
 	copy(myList2.begin(), myList2.end(), ostream_iterator<long>(cout, " "));
-
+	cout << "\n";
 
 	system("pause");
 
